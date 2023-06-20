c5113dd1983b6ac1c1c9b18186c349c2eb6f7beb
Trần Đình Nam (ASIA) <namtd@aht-asia.com>
2023-06-20 13:44:03 +0700

    base : init project

---
+++ README.md
@@ -0,0 +1,29 @@
+## Features
+
+TODO: List what your package can do. Maybe include images, gifs, or videos.
+
+## Getting started
+
+TODO: List prerequisites and provide or point to information on how to
+start using the package.
+
+## Usage
+
+- Add the following code to the file `pubspec.yaml` then run terminal `flutter pub get`
+
+```yaml
+dependencies:
+  flutter:
+    sdk: flutter
+
+  flutter_base_core:
+    git:
+      url: https://github.com/NamTran20102/flutter_base.git
+      ref: main
+```
+
+## Additional information
+```text
++ url: This is the remote path
++ ref: It can be branch name, commit reference id or version
+```
---
+++ lib/core/enums/base_enum.dart
@@ -0,0 +1,56 @@
+import 'package:path/path.dart' as path;
+
+enum EnumAppEnvironment { dev, stag, product, test }
+
+enum EnumDataSource { asset, network, file, contentUri, unknown }
+
+enum EnumSpeed { veryFast, fast, slow }
+
+enum EnumStatus { success, error, warn, info }
+
+enum EnumTypeFile {
+  isVideo,
+  isImage,
+  isAudio,
+  isPdf,
+  isPpt,
+  isDoc,
+  isExcel,
+  isUnknown;
+
+  static EnumTypeFile checkTypeFile({required String filePath}) {
+    final extension = path.extension(filePath).toLowerCase();
+    if (extension == '.jpg' ||
+        extension == '.jpeg' ||
+        extension == '.png' ||
+        extension == '.gif' ||
+        extension == '.webp') {
+      return EnumTypeFile.isImage;
+    } else if (extension == '.mp4' ||
+        extension == '.avi' ||
+        extension == '.mov' ||
+        extension == '.wmv' ||
+        extension == '.flv' ||
+        extension == '.mkv') {
+      return EnumTypeFile.isVideo;
+    } else if (extension == '.doc' ||
+        extension == '.docx' ||
+        extension == '.odt' ||
+        extension == '.rtf') {
+      return EnumTypeFile.isDoc;
+    } else if (extension == '.pdf') {
+      return EnumTypeFile.isPdf;
+    } else if (extension == '.ppt' || extension == '.pptx') {
+      return EnumTypeFile.isPpt;
+    } else if (extension == '.xls' ||
+        extension == '.xlsx' ||
+        extension == '.ods' ||
+        extension == '.csv') {
+      return EnumTypeFile.isExcel;
+    } else if (extension == '.mp3') {
+      return EnumTypeFile.isAudio;
+    } else {
+      return EnumTypeFile.isUnknown;
+    }
+  }
+}
